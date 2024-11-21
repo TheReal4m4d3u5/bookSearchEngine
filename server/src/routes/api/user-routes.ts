28e508463adf11a22816bdadbdbcb2a1921faf34
Avery Jacobson <averyajacobson@gmail.com>
2024-11-20 19:36:38 -0600

    udpated user server routes

--- server/src/routes/api/user-routes.ts
+++ server/src/routes/api/user-routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import User from "../../models/User"
+
 const router = express.Router();
 import {
   createUser,
@@ -19,5 +21,19 @@ router.route('/login').post(login);
 router.route('/me').get(authenticateToken, getSingleUser);
 
 router.route('/books/:bookId').delete(authenticateToken, deleteBook);
+
+
+router.post('/users', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Add logic to save user to the database
+    const newUser = await User.create({ username, email, password });
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
 
 export default router;
