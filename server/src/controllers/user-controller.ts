88d7fb44469371b4cf0e77a4cfa10a3688793181
Avery Jacobson <averyajacobson@gmail.com>
2024-11-22 22:40:20 -0600

    took out create and login controllers

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from 'express';
 // import user model
 import User from '../models/User.js';
 // import sign token function from auth
-import { signToken } from '../services/auth.js';
+// import { signToken } from '../services/auth.js';
 
 // get a single user by either their id or their username
 export const getSingleUser = async (req: Request, res: Response) => {
@@ -18,32 +18,32 @@ export const getSingleUser = async (req: Request, res: Response) => {
 };
 
 // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
-export const createUser = async (req: Request, res: Response) => {
-  const user = await User.create(req.body);
+// export const createUser = async (req: Request, res: Response) => {
+//   const user = await User.create(req.body);
 
-  if (!user) {
-    return res.status(400).json({ message: 'Something is wrong!' });
-  }
-  const token = signToken(user.username, user.password, user._id);
-  return res.json({ token, user });
-};
+//   if (!user) {
+//     return res.status(400).json({ message: 'Something is wrong!' });
+//   }
+//   const token = signToken(user.username, user.password, user._id);
+//   return res.json({ token, user });
+// };
 
 // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
 // {body} is destructured req.body
-export const login = async (req: Request, res: Response) => {
-  const user = await User.findOne({ $or: [{ username: req.body.username }, { email: req.body.email }] });
-  if (!user) {
-    return res.status(400).json({ message: "Can't find this user" });
-  }
+// export const login = async (req: Request, res: Response) => {
+//   const user = await User.findOne({ $or: [{ username: req.body.username }, { email: req.body.email }] });
+//   if (!user) {
+//     return res.status(400).json({ message: "Can't find this user" });
+//   }
 
-  const correctPw = await user.isCorrectPassword(req.body.password);
+//   const correctPw = await user.isCorrectPassword(req.body.password);
 
-  if (!correctPw) {
-    return res.status(400).json({ message: 'Wrong password!' });
-  }
-  const token = signToken(user.username, user.password, user._id);
-  return res.json({ token, user });
-};
+//   if (!correctPw) {
+//     return res.status(400).json({ message: 'Wrong password!' });
+//   }
+//   const token = signToken(user.username, user.password, user._id);
+//   return res.json({ token, user });
+// };
 
 // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
 // user comes from `req.user` created in the auth middleware function
