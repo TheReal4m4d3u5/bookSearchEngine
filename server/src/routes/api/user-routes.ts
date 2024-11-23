78ad65e9596654d89beb6307ad1e5c3f78e8edad
Avery Jacobson <averyajacobson@gmail.com>
2024-11-22 22:29:01 -0600

    removed apis

--- server/src/routes/api/user-routes.ts
+++ server/src/routes/api/user-routes.ts
@@ -1,39 +1,39 @@
-//import express from 'express';
-//import User from "../../models/User"
+import express from 'express';
+import User from "../../models/User"
 
-// const router = express.Router();
-// import {
-//   createUser,
-//   getSingleUser,
-//   saveBook,
-//   deleteBook,
-//   login,
-// } from '../../controllers/user-controller.js';
+const router = express.Router();
+import {
+  createUser,
+  getSingleUser,
+  saveBook,
+  deleteBook,
+  login,
+} from '../../controllers/user-controller.js';
 
-// // import middleware
-// import { authenticateToken } from '../../services/auth.js';
+// import middleware
+import { authenticateToken } from '../../services/auth.js';
 
-// // put authMiddleware anywhere we need to send a token for verification of user
-// router.route('/').post(createUser).put(authenticateToken, saveBook);
+// put authMiddleware anywhere we need to send a token for verification of user
+router.route('/').post(createUser).put(authenticateToken, saveBook);
 
-// router.route('/login').post(login);
+router.route('/login').post(login);
 
-// router.route('/me').get(authenticateToken, getSingleUser);
+router.route('/me').get(authenticateToken, getSingleUser);
 
-// router.route('/books/:bookId').delete(authenticateToken, deleteBook);
+router.route('/books/:bookId').delete(authenticateToken, deleteBook);
 
 
-// router.post('/users', async (req, res) => {
-//   try {
-//     const { username, email, password } = req.body;
+router.post('/users', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
 
-//     // Add logic to save user to the database
-//     const newUser = await User.create({ username, email, password });
+    // Add logic to save user to the database
+    const newUser = await User.create({ username, email, password });
 
-//     res.status(201).json(newUser);
-//   } catch (error) {
-//     res.status(500).json({ message: 'Internal Server Error' });
-//   }
-// });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
 
-// export default router;
+export default router;
